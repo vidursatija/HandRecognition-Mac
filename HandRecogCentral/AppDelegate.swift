2452add8b7fa7ff66ff8ed09c3799355c2f19003
vidursatija <vidursatija@gmail.com>
2016-10-15 14:01:12 +0530

    Initial Commit

---
+++ HandRecogCentral/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HandRecogCentral
+//
+//  Created by Vidur Satija on 15/10/16.
+//  Copyright © 2016 Aromatic Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HandRecogCentral/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HandRecogCentral
+//
+//  Created by Vidur Satija on 15/10/16.
+//  Copyright © 2016 Aromatic Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
